679fbb3dd951da6f9879003c21509aa86b9f5cdb
Tom Robertshaw <me@tomrobertshaw.net>
2017-01-18 21:52:37 +0000

    Add service worker to provide offline backup when cached.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@
 ---
 {% include_relative loadCSS.min.js %}
 {% include_relative cssrelpreload.min.js %}
+{% include_relative service-worker-setup.js %}
---
+++ service-worker.js
@@ -0,0 +1,117 @@
+---
+layout: null
+---
+
+'use strict';
+
+const cacheVersion = {{'now' | date: '%s' }};
+const offlineCache = 'offline-' + cacheVersion;
+const offlinePage = '/offline/';
+
+/**
+ * Delete caches that do not match the current version of the service worker.
+ *
+ * @returns {*|Promise.<TResult>}
+ */
+function clearOldCaches() {
+    return caches.keys().then(keys => {
+        return Promise.all(
+            keys
+                .filter(key => key.indexOf(cacheVersion) !== 0)
+                .map(key => caches.delete(key))
+        );
+    });
+}
+
+function cacheOfflinePage() {
+    console.log('In offline cache function');
+    return caches.open(offlineCache)
+        .then(cache => {
+            console.log("adding offline page to cache: " + offlinePage);
+            return cache.addAll([offlinePage]);
+        })
+        .then(function() {
+            console.log('WORKER: install completed');
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+/**
+ * Install Service Worker
+ */
+ self.addEventListener('install', event => {
+    event.waitUntil(
+        cacheOfflinePage()
+        .then( () => self.skipWaiting() )
+    );
+});
+
+/**
+ * Activate Service Worker
+ */
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        clearOldCaches()
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    let request = event.request;
+
+    // Ignore non-GET requests
+    if (request.method !== 'GET') {
+        return;
+    }
+
+    // For HTML requests, try the network first, fall back to the cache, finally the offline page
+    if (request.headers.get('Accept').indexOf('text/html') !== -1) {
+        event.respondWith(
+            fetch(request)
+                .then( response => {
+                    // NETWORK
+                    // Stash a copy of this page in the pages cache
+                    let copy = response.clone();
+                    console.log("Adding page to cache: " + request.url);
+                    caches.open(offlineCache)
+                        .then( cache => cache.put(request, copy) );
+
+                    return response;
+                })
+                .catch( () => {
+                    // CACHE or FALLBACK
+                    return caches.match(request)
+                        .then( response => response || caches.match('/offline') );
+                })
+        );
+        return;
+    } else {
+        // For non-HTML requests, look in the cache first, fall back to the network
+        event.respondWith(
+            caches.match(request)
+                .then(response => {
+                    // CACHE
+                    return response || fetch(request)
+                        .then( response => {
+                            // NETWORK
+                            // If the request is for an image, stash a copy of this image in the images cache
+                            if (response && response.ok) {
+                                let copy = response.clone();
+                                caches.open(offlineCache)
+                                    .then( cache => cache.put(request, copy) );
+                            }
+                            return response;
+                        })
+                        .catch( () => {
+                            // OFFLINE
+                            // If the request is for an image, show an offline placeholder
+                            if (request.headers.get('Accept').indexOf('image') !== -1) {
+                                return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml'}});
+                            }
+                        });
+                })
+        );
+    }
+});
