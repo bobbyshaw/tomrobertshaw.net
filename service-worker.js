abd25b79009e2c6b5777cc061fe9b6b256d9c972
Tom Robertshaw <me@tomrobertshaw.net>
2017-01-24 11:22:19 +0000

    Change service worker to respond from cache all the time, not just for assets.
    Added debug mode for logging.
    Fixed cache cleaning key.

--- service-worker.js
+++ service-worker.js
@@ -7,6 +7,7 @@ layout: null
 const cacheVersion = {{'now' | date: '%s' }};
 const offlineCache = 'offline-' + cacheVersion;
 const offlinePage = '/offline/';
+const debugMode = false;
 
 /**
  * Delete caches that do not match the current version of the service worker.
@@ -14,27 +15,24 @@ const offlinePage = '/offline/';
  * @returns {*|Promise.<TResult>}
  */
 function clearOldCaches() {
+    debug("Clean old caches");
     return caches.keys().then(keys => {
         return Promise.all(
             keys
-                .filter(key => key.indexOf(cacheVersion) !== 0)
+                .filter(key => key.indexOf(offlineCache) !== 0)
                 .map(key => caches.delete(key))
         );
     });
 }
 
 function cacheOfflinePage() {
-    console.log('In offline cache function');
+    debug("Cache offline page");
     return caches.open(offlineCache)
         .then(cache => {
-            console.log("adding offline page to cache: " + offlinePage);
             return cache.addAll([offlinePage]);
         })
-        .then(function() {
-            console.log('WORKER: install completed');
-        })
         .catch(error => {
-            console.log(error);
+            debug(error);
         })
 }
 
@@ -42,6 +40,7 @@ function cacheOfflinePage() {
  * Install Service Worker
  */
  self.addEventListener('install', event => {
+    debug("Installing Service Worker");
     event.waitUntil(
         cacheOfflinePage()
         .then( () => self.skipWaiting() )
@@ -52,6 +51,7 @@ function cacheOfflinePage() {
  * Activate Service Worker
  */
 self.addEventListener('activate', event => {
+    debug("Activating Service Worker");
     event.waitUntil(
         clearOldCaches()
             .then(() => self.clients.claim())
@@ -63,55 +63,48 @@ self.addEventListener('fetch', event => {
 
     // Ignore non-GET requests
     if (request.method !== 'GET') {
+        debug("Ignoring non GET request");
         return;
     }
 
-    // For HTML requests, try the network first, fall back to the cache, finally the offline page
-    if (request.headers.get('Accept').indexOf('text/html') !== -1) {
-        event.respondWith(
-            fetch(request)
-                .then( response => {
-                    // NETWORK
-                    // Stash a copy of this page in the pages cache
-                    let copy = response.clone();
-                    console.log("Adding page to cache: " + request.url);
-                    caches.open(offlineCache)
-                        .then( cache => cache.put(request, copy) );
+    event.respondWith(
+        caches.match(request)
+            .then(response => {
+                // CACHE
+                debug("Fetching " + request.url)
 
-                    return response;
-                })
-                .catch( () => {
-                    // CACHE or FALLBACK
-                    return caches.match(request)
-                        .then( response => response || caches.match(offlinePage) );
-                })
-        );
-        return;
-    } else {
-        // For non-HTML requests, look in the cache first, fall back to the network
-        event.respondWith(
-            caches.match(request)
-                .then(response => {
-                    // CACHE
-                    return response || fetch(request)
-                        .then( response => {
-                            // NETWORK
-                            // If the request is for an image, stash a copy of this image in the images cache
-                            if (response && response.ok) {
-                                let copy = response.clone();
-                                caches.open(offlineCache)
-                                    .then( cache => cache.put(request, copy) );
-                            }
-                            return response;
-                        })
-                        .catch( () => {
-                            // OFFLINE
-                            // If the request is for an image, show an offline placeholder
-                            if (request.headers.get('Accept').indexOf('image') !== -1) {
-                                return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml'}});
-                            }
-                        });
-                })
-        );
-    }
+                if (response) {
+                    debug("Found in cache: " + request.url);
+                }
+
+                return response || fetch(request)
+                    .then( response => {
+                        // NETWORK
+                        debug("Going to network: " + request.url);
+                        if (response && response.ok) {
+                            debug("Saving in cache: " + request.url);
+                            let copy = response.clone();
+                            caches.open(offlineCache)
+                                .then( cache => cache.put(request, copy) );
+                        }
+                        return response;
+                    })
+                    .catch( error => {
+                        // OFFLINE
+                        debug("Offline and no cache for: " + request.url + ": " + error);
+                        if (request.mode == 'navigate') {
+                            debug("Showing offline page")
+                            return caches.match(offlinePage);
+                        } else if (request.headers.get('Accept').indexOf('image') !== -1) {
+                            return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml'}});
+                        }
+                    });
+            })
+    );
 });
+
+function debug(message) {
+    if (debugMode) {
+        console.log(message);
+    }
+}
