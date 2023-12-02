bcb0f7c816ddb586d691912179cb0c6cd05e329d
Tom Robertshaw <tom.robertshaw@space48.com>
2023-12-02 14:59:02 +0000

    Unregister service worker.

--- js/service-worker-setup.js
+++ js/service-worker-setup.js
@@ -13,13 +13,11 @@ function ServiceWorkerSetup() {
 ServiceWorkerSetup.prototype.init = function () {
     // Register the service worker
     if ('serviceWorker' in navigator) {
-    	navigator.serviceWorker.register('/service-worker.js')
-        .then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }).catch(function(err) {
-        // registration failed :(
-        	console.log('ServiceWorker registration failed: ', err);
+        navigator.serviceWorker.getRegistrations()
+        .then( function(registrations) {
+            for(let registration of registrations) {
+                registration.unregister();
+            }
         });
     }
 };
